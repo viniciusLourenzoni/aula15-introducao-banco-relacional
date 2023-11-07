8c593eaa9babc3ecbe93cb5012a579f830f63cdc
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2023-11-06 23:58:08 -0300

    feat: new routes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+from http import HTTPStatus
 from typing import Optional
 
 from fastapi import FastAPI
-from sqlmodel import Field, Session, SQLModel, create_engine
+from fastapi.responses import JSONResponse
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 # ORM - Object Relational Mapping
 # placa
@@ -57,3 +60,61 @@ def cria_registro(manutencao: Manutencao):
         session.commit()
         session.refresh(manutencao)
         return manutencao
+
+
+@app.patch("/manutencoes/{id}/finalizar")
+def finalizar_manutencao(id: int):
+    with Session(engine) as session:
+        # Monta a query = SELECT * FROM manutencao where id = id
+        statement = select(Manutencao).where(Manutencao.id == id)
+        manutencao = session.exec(statement=statement).first()
+        # validar se a manutenção ainda está em aberto
+        if manutencao.data_finalizacao:
+            return JSONResponse(
+                content={"message": "Manutenção já finalizada"},
+                status_code=HTTPStatus.BAD_REQUEST,
+            )
+
+        # atualizar a data_finalizacao com a data atual
+        manutencao.data_finalizacao = str(datetime.now())
+        # aplica a alteração no banco
+        session.commit()
+        # Atualiza o registro a partir
+        session.refresh(manutencao)
+        return manutencao
+
+
+# DELETE /manutencoes/id
+# Só pode deletar uma manutencao que ainda não tenha sido finalizada
+
+
+@app.delete("/manutencoes/{id}")
+def deletar_manutencao(id: int):
+    with Session(engine) as session:
+        # Monta a query = SELECT * FROM manutencao where id = id
+        statement = select(Manutencao).where(Manutencao.id == id)
+
+        # Executa a query e retorna o registro ou None se não existir
+        manutencao = session.exec(statement=statement).first()
+
+        print(manutencao)
+        # Valida se existe a manutenção
+        if not manutencao:
+            return JSONResponse(
+                content={"message": "Manutenção não existe"},
+                status_code=HTTPStatus.NOT_FOUND,
+            )
+
+        # Valida se a manutenção já foi finalizada
+        if manutencao.data_finalizacao:
+            return JSONResponse(
+                content={"message": "Manutenção já finalizada"},
+                status_code=HTTPStatus.BAD_REQUEST,
+            )
+
+        session.delete(manutencao)
+        session.commit()
+        return JSONResponse(
+            content=None,
+            status_code=HTTPStatus.NO_CONTENT,
+        )
